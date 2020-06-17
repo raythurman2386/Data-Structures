e9b8b07a7dd285098724cf768fddb3999488c98e
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-16 21:57:43 -0400

    re implemented properly, tests passing

--- stack/stack.py
+++ stack/stack.py
@@ -11,91 +11,65 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
-'''
- Node implementation from what I have done previously with college
-
- TODO: Once lecture and guided project is over, revisit and refactor
- as needed. All current tests for this file pass with both
- implementations
-'''
-
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None, next=None):
         self.value = value
-        self.next = None # Single next denotes singly linked list
-
-    # Return the value of current Node
-    def get_data(self):
-        return self.value
-
-    # Set the Current Nodes value
-    def set_data(self, value):
-        self.value = value
-
-    # Get the next node in the List
-    def get_next(self):
-        return self.next
-
-    # Set the next node in the list
-    def set_next(self, next):
-        self.next = next
+        self.next = next # Single next denotes singly linked list
 
 
-'''
- Linked list implementation
- TODO: Refactor after lecture and guided project
- Initial Implementation passes tests but I want
- To see what we come up with tonight.
- This is slightly different than the linked lists
- I've done before so I could get the tests passing
-'''
+# Linked list implementation
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-class Stack:
-    def __init__(self, head=None):
-        self.head = head
-        self.count = 0
-        self.storage = []
-
-    # Ability to return the length of the list
-    def __len__(self):
-        return self.count
-
-    # Insert a new Node into the list
-    def push(self, data):
+    # Insert a new Node into the front list
+    def add_to_head(self, data):
         new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-        self.storage.append(new_node)
-        self.count += 1
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
         return self
 
-    # delete an item at a given index
+    # delete an item from the end
+    def remove_head(self):
+        if not self.head:
+            return None
+
+        if self.head.next is None:
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
+
+        head_value = self.head.value
+        self.head = self.head.next
+        return head_value
+
+
+# Initial stack class, part one of section
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+        return self
+
     def pop(self):
-        if self.count == 0:
+        if self.size == 0:
             return
         else:
-            self.count -= 1
-            return self.storage.pop().value
-
-
-#  Array Implementation
-
-# class Stack:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
-
-#     def __len__(self):
-#         return self.size
-
-#     def push(self, value):
-#         self.storage.append(value)
-#         self.size += 1
-#         return self
-
-#     def pop(self):
-#         if self.size == 0:
-#             return
-#         else:
-#             self.size -= 1
-#             return self.storage.pop()
+            removed_value = self.storage.head.value
+            self.storage.remove_head()
+            self.size -= 1
+            return removed_value
