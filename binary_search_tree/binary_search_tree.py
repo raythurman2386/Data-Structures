4568e090e128672bab9f8f7f43d2204dcf8a92e3
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-22 21:46:26 -0400

    pseudo code from lecture/contains func

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,12 +19,46 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
+        # Take the value of the current node (self.value)
+        # COmpare to the value that we are inserting
+
+        # if new_value < self.value
+        # if self.left is taken by a node,
+        # make that node, call insert
+        # self.left.insert(value)
+        # Set the left to the new node
+        # self.left = BSTNode(value)
+        # if new_value > self.value
+        # if self.right is taken,
+        # make self.right call insert
+        # self.right.insert(value)
+        # set the right value to the new node
+        # self.right = BSTNode(value)
         pass
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+
+        # Compare the target to the current value
+        # if current value < target
+        found = False
+
+        if self.value < target:
+            # check the left subtree
+            if self.left is None:
+                return False
+            found = self.left.contains(target)
+
+        if self.value >= target:
+            # check the right subtree
+            if self.right is None:
+                return False
+            found = self.right.contains(target)
+
+        return found
 
     # Return the maximum value found in the tree
     def get_max(self):
